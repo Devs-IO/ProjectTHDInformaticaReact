767b2484196ec1da7c5181aa06691325c4ce2d0e
rebecanonato89 <rebecanonato89@gmail.com>
2022-02-14 09:01:44 -0300

    feat: create-client

--- src/pages/ClientRegister/index.tsx
+++ src/pages/ClientRegister/index.tsx
@@ -1,9 +1,10 @@
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { useCallback, useRef } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { BsFillXCircleFill, BsFillCheckCircleFill } from 'react-icons/bs';
+import api from '../../services/api';
 
 import getValidationErrors from 'utils/getValidationErrors';
 import Button from 'components/Button';
@@ -16,9 +17,15 @@ import Input from 'components/Input';
 export const ClientRegister = () => {
   const formRef = useRef<FormHandles>(null);
   const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [city, setCity] = useState('');
 
   const handleRegister = useCallback(async (data: any) => {
     try {
+      await api.post('/clients', data);
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -37,23 +44,55 @@ export const ClientRegister = () => {
         <Form ref={formRef} onSubmit={handleRegister}>
           <div>
             <label>Nome do Cliente</label>
-            <Input name="search" placeholder="Nome do Cliente" required />
+            <Input
+              name="name"
+              placeholder="Nome do Cliente"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
           </div>
           <div>
             <label>Telefone</label>
-            <Input name="phone" placeholder="Telefone" required />
+            <Input
+              name="phone"
+              placeholder="Telefone"
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
           </div>
           <div>
             <label>Email</label>
-            <Input name="email" placeholder="Email" />
+            <Input
+              name="email"
+              placeholder="Email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
             <label>CPF</label>
-            <Input name="cpf" placeholder="xxx.xxx.xxx-xx" />
+            <Input
+              name="cpf"
+              placeholder="xxx.xxx.xxx-xx"
+              type="text"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+            />
           </div>
           <div>
             <label>Cidade</label>
-            <Input name="city" placeholder="Cidade" />
+            <Input
+              name="city"
+              placeholder="Cidade"
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
           </div>
 
           <Button type="submit">
